b344c47ee3045ccb81737a138d16cd8b2b03adad
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-26 09:59:42 +0200

    show how to add and remove elements. And that click events can be used on "non-buttons".

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let inputElement = document.getElementById("text-input")
 let buttonElementAdd = document.getElementById("add-text")
 let textContainer = document.getElementById("text-container")
+//
 
 // event & event handler functions
 buttonElementAdd.addEventListener("click", addText)
@@ -13,4 +14,36 @@ function addText() {
 
     // textContainer = textContainer.textContent + "<h1>" + text
     textContainer.innerHTML += "<p> " + text + "</p>"
+
+    // try to remove someting
+    //inputElement.remove()
+    //buttonElementAdd.remove()
+   
+   //textContainer.remove()
+
+   // remove a specific paragraph:
+
+}
+
+/*
+    <div id="text-container">
+
+        Text inni divven :)
+
+        <p>mer text inni en p-tag inni divven</p>
+        <p>enda mer text inni enda en p-tag inni divven</p>
+    </div>
+*/
+
+
+// find the paragraph with id "test-text"
+let firstParagraphElement = document.getElementById("item-1")
+
+//paragraphElement.remove()
+
+firstParagraphElement.addEventListener("click", removeElement)
+
+// removes a given element from the page
+function removeElement() {
+    firstParagraphElement.remove()
 }
