d0ac4f9057d4ec9d1f516995fd302ff36a41ebc5
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-26 11:23:47 +0200

    clear input after submission

--- js/script.js
+++ js/script.js
@@ -9,20 +9,16 @@ buttonElementAdd.addEventListener("click", addText)
 
 // add text function reads value from the input field and then adds it to the div container
 function addText() {
- 
     let text = inputElement.value // get the text from the input element
-
-    // recreate the innerHTML line with createElement:
-
-    // 1. Create an element in memory, and store it in a variable:
+    
     let paragraphElement = document.createElement("p")
-    // 2. change its text value
     paragraphElement.textContent = text
-    // 3. add the element to the page
-    textContainer.append(paragraphElement) // to add elements we must use .append (since we use createElement to create it)
-    // 4. add a "click" event listener to the paragraphElement, so it can be removed
     paragraphElement.addEventListener("click", removeElement)
-
+    
+    textContainer.append(paragraphElement)
+    
+    // clean up the input field
+    inputElement.value = ""
 }
 
 // remove element based on given event-argument 
